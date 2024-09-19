50ccc92547fd6830d44563025ba15c412fef8cb5
montonurb <brunomoncar007@gmail.com>
2024-09-19 18:31:55 -0300

    Criando o projeto Todo List

---
+++ src/main/java/com/montonurb/desafio_todolist/entity/Todo.java
@@ -0,0 +1,52 @@
+package com.montonurb.desafio_todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String check;
+    private Integer priority;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getCheck() {
+        return check;
+    }
+    public void setCheck(String check) {
+        this.check = check;
+    }
+    public Integer getPriority() {
+        return priority;
+    }
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    
+}
